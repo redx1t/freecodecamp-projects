2c31113c9da535ac64d1e04c54b47f3679ff185e
redx1t <68523591+redx1t@users.noreply.github.com>
2021-11-26 06:10:32 +0300

    Telephone Number Validator

---
+++ Telephone Number Validator/index.js
@@ -0,0 +1,9 @@
+function telephoneCheck(str) {
+
+    //regex to check
+    var regex = /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
+    //test for boolean return
+    return regex.test(str);
+}
+
+console.log(telephoneCheck("555-555-5555"));
